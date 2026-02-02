2cdfd3960208189b46f5cba6d7f62e9ad0d14922
Senia <seniakonectea@gmail.com>
2026-02-02 13:16:13 +0100

    terminamos la sesión del lunes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,31 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+
+    if request.method == "POST":
+        usuario = request.form["user"]
+        password = request.form["password"]
+        Email = request.form["Email"]
+        Color = request.form["Color"]
+
+
+        print("usuario ingresado:", usuario)
+        print("password ingresado:", password)
+
+
+        return  render_template("user.html", usuario=usuario,Email=Email, Color=Color)
+    #f"<p>usuario {usuario} ha intentado iniciar sesión. </p>"
+
+
     return render_template("login.html")
+
+@app.route("/user") 
+def hello_user():
+    return "<p>Hello, Usuario!</p>"
+
+@app.route("/user/logged")
+def logged_user():
+    return render_template("User_Custom.html")
+
